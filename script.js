a59fdc20570864c4818d654a9cf8fb1a9ccc37dd
Drew Carrillo <drewc3@gmail.com>
2020-11-22 13:30:42 -0700

    player position and collision

--- script.js
+++ script.js
@@ -25,12 +25,16 @@ canvas.addEventListener('mouseup', function () {
 });
 
 // Player
+const playerLeft = new Image()
+playerLeft.src='fish_swim_left.png'
+const playerRight = new Image()
+playerRight.src = 'fish_swim_right.png'
 class Player {
   constructor() {
     this.x = canvas.width;
     this.y = canvas.height / 2;
     this.radius = 50;
-    this.angle = 0;
+    this.angle = 20;
     this.frameX = 0;
     this.frameY = 0;
     this.frame = 0;
@@ -39,8 +43,9 @@ class Player {
   }
   update() {
     const dx = this.x - mouse.x;
-    console.log(canvas.width, mouse.x)
     const dy = this.y - mouse.y;
+    let theta = Math.atan2(dy,dx)
+    this.angle = theta
     if (mouse.x != this.x) {
       this.x -= dx/30;
     }
@@ -62,17 +67,90 @@ class Player {
     ctx.fill()
     ctx.closePath()
     ctx.fillRect(this.x, this.y, this.radius, 10)
+
+    ctx.save()
+    ctx.translate(this.x, this.y)
+    ctx.rotate(this.angle)
+    if (this.x >= mouse.x) {
+    ctx.drawImage(playerLeft, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, 0-60, 0-45, this.spriteWidth/4, this.spriteHeight/4)
+    } else {
+      ctx.drawImage(playerRight, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, 0-60, 0-45, this.spriteWidth/4, this.spriteHeight/4)
+    }
+    ctx.restore()
   }
 }
 const player = new Player()
 
 // Bubbles
+const bubblesArray = []
+class Bubble {
+  constructor() {
+    this.x = Math.random() * canvas.width
+    this.y = canvas.height +100
+    this.radius = 50
+    this.speed = Math.random() *5 +1
+    this.distance
+    this.counted = false
+    this.sound = Math.random() <=0.5 ? 'sound1' : 'sound2'
+  }
+  update(){
+    this.y -= this.speed
+    const dx = this.x - player.x
+    const dy = this.y - player.y
+    this.distance = Math.sqrt(dx*dx +dy*dy)
+  }
+  draw(){
+    ctx.fillStyle='blue'
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0,Math.PI *2)
+    ctx.fill()
+    ctx.closePath()
+    ctx.stroke()
+  }
+}
 
+const bubblePop1 = document.createElement('audio')
+bubblePop1.src = 'bubbles-single1.wav'
+const bubblePop2 = document.createElement('audio')
+bubblePop2.src = 'bubbles-single2.wav'
+
+function handleBubbles(){
+  if (gameFrame%50 === 0){
+    bubblesArray.push(new Bubble())
+    console.log(bubblesArray.length)
+  }
+  for (let i=0; i<bubblesArray.length; i++){
+    bubblesArray[i].update()
+    bubblesArray[i].draw()
+    } // separate for loop to prevent bubble blinking
+  for (let i=0; i<bubblesArray.length; i++){
+    if (bubblesArray[i].y <0-this.radius *2) bubblesArray.splice(i,1)
+    if(bubblesArray[i]){
+      if (bubblesArray[i].distance < bubblesArray[i].radius+player.radius){ //collision
+        if (!bubblesArray[i].counted){
+          if (bubblesArray[i].sound === 'sound1'){
+            //bubblePop1.play()
+            console.log('sound 1')
+          }else {
+            //bubblePop2.play()
+          }
+          score++
+          bubblesArray[i].counted = true
+          bubblesArray.splice(i,1)
+        }
+      }
+    }
+  }
+}
 // Animation Loop
 function animate() {
   ctx.clearRect(0,0,canvas.width, canvas.height)
+  handleBubbles()
   player.update()
   player.draw()
+  ctx.fillStyle = 'black'
+  ctx.fillText('Score: '+ score, 10, 50)
+  gameFrame++
   requestAnimationFrame(animate)
 }
 animate()
